dc38318cf5e8bc15268b495b87910a305300f56a
mliu947 <mingmingliu3517@gmail.com>
2025-03-15 15:34:09 +1300

    css for background image

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,8 @@ function Game() {
     const moneyCount = Math.floor(Math.random() * 6) + 5;
     const newMoneyDrops = Array.from({ length: moneyCount }, (_, index) => ({
       id: index,
-      x: Math.random()* 300 - 50,
-      y: Math.random() * 300 - 100,
+      x: Math.random()* 300 + 50,
+      y: Math.random() * 100 + 50,
       duration: (Math.random() * 1.5 + 1).toFixed(2)
     }));
 
@@ -109,6 +109,7 @@ function Game() {
 
       <img src={backgroundPic}
         alt="forest"
+        className='background-image'
         style={{ width: "100%", height: "auto", marginTop: "20px" }}
       />
 
@@ -119,7 +120,7 @@ function Game() {
         </button>
 
         {lastEarnings !== null && (
-          <span  className={`earnings-text ${showEarnings ? "fade-in" : "fade-out"}`}>
+          <span  className={`earnings-text`}>
             +${lastEarnings}
           </span>
         )}
